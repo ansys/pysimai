f9a5b04514ea0cdebc3970d35dc45a4d2a5eef88
kliment-slice <61919865+kliment-slice@users.noreply.github.com>
2024-10-02 09:19:30 -0700

    Check geometry generation function has a suitable signature (optimization) (#90)

--- src/ansys/simai/core/data/optimizations.py
+++ src/ansys/simai/core/data/optimizations.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 
 import logging
+from inspect import signature
 from typing import Callable, Dict, List, Optional, Tuple
 
 from tqdm import tqdm
@@ -161,6 +162,7 @@ class OptimizationDirectory(Directory[Optimization]):
             print(results)
         """
         workspace_id = get_id_from_identifiable(workspace, True, self._client._current_workspace)
+        _check_geometry_generation_fn_signature(geometry_generation_fn, geometry_parameters)
         if not minimize and not maximize:
             raise InvalidArguments("No global coefficient to optimize.")
         objective = {}
@@ -219,6 +221,14 @@ class OptimizationDirectory(Directory[Optimization]):
                 logger.debug("Optimization complete.")
                 progress_bar.set_description("Optimization complete.")
             return iterations_results
+
+
+def _check_geometry_generation_fn_signature(geometry_generation_fn, geometry_parameters):
+    geometry_generation_fn_args = signature(geometry_generation_fn).parameters
+    if geometry_generation_fn_args.keys() != geometry_parameters.keys():
+        raise InvalidArguments(
+            f"geometry_generation_fn requires the following signature: {list(geometry_parameters.keys())}, but got: {list(geometry_generation_fn_args.keys())}"
+        )
 
 
 # Undocumented for now, users don't really need to interact with it
