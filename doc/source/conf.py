24d9cbbc1f0f4fa7cfcedc1c1f54e6da28c770a6
Maid Sultanovic <maid.sultanovic@ansys.com>
2025-06-02 15:37:38 +0200

    Add pydantic to intersphinx mapping

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -84,6 +84,7 @@ sphinx_gallery_conf = {
 }
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
+    "pydantic": ("https://docs.pydantic.dev/latest/", None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
